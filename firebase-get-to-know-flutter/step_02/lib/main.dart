3114596687abe8aeea5b8fb061117cdbe4233295
KodPlanet <kodplanet@gmail.com>
2021-06-24 23:23:02 +0300

    Add packages

--- firebase-get-to-know-flutter/step_02/lib/main.dart
+++ firebase-get-to-know-flutter/step_02/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'src/widgets.dart';
 
@@ -51,7 +52,7 @@ class HomePage extends StatelessWidget {
           ),
           const Header("What we'll be doing"),
           const Paragraph(
-            'Join us for a day full of Firebase Workshops and Pizza!',
+            'Join us for a day full of Firebase Workshops and Lahmacun!',
           ),
         ],
       ),
